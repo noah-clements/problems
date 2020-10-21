1978c0ae239f8ba982b56782cd521e910b1c25a2
noah-clements <71468308+noah-clements@users.noreply.github.com>
2020-10-21 14:33:06 -0400

    Update __init__.py
    Hopefully allowed lock_pairs2 test to print full output so I can see which pair caused a problem

--- tideman/__init__.py
+++ tideman/__init__.py
@@ -98,8 +98,9 @@ def lock_pairs1():
 @check50.hidden("lock_pairs did not correctly lock all non-cyclical pairs")
 def lock_pairs2():
     """lock_pairs skips final pair if it creates cycle"""
+    check50.run("./tideman_test 6 14").stdout();
     check50.run("./tideman_test 6 14").stdout("false true false false false false false false false false true false false false false false false false false false false false false true false false true true false false false false false false false false ").exit(0)
-
+    
 @check50.check(compiles)
 @check50.hidden("lock_pairs did not correctly lock all non-cyclical pairs")
 def lock_pairs3():
